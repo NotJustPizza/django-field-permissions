807c58d33e12aac6cb4b4dd44354b3c9cfc2b921
Sleepy105 <lm.sousa@fe.up.pt>
2019-08-12 16:54:50 +0100

    Fix serializer bug

--- field_permissions/api/rest_framework.py
+++ field_permissions/api/rest_framework.py
@@ -19,10 +19,10 @@ class FieldPermissionSerializerMixin:
         model_field_names = [f.name for f in model._meta.get_fields()]  # this might be too broad
         for name in model_field_names:
             if name in self.fields:
-                if not self.instance.has_field_perm(user, field=name, operation='view'):
+                if not model.has_field_perm(model, user=user, field=name, operation='view'):
                     self.fields.remove(name)
                     continue
-                if not self.instance.has_field_perm(user, field=name, operation='change'):
+                if not model.has_field_perm(model, user=user, field=name, operation='change'):
                     self.fields[name].read_only = True
 
 
